34767773b7c5a1ebc484d226bb524454dd750fd3
smshuvo100 <smshuvo100@gmail.com>
2026-04-21 16:46:39 +0600

    2nd

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,10 +1,15 @@
 import { Link } from "expo-router";
-import { Text, View } from "react-native";
+import { styled } from "nativewind";
+import { Text } from "react-native";
+
+import { SafeAreaView as RNSafeAreaView } from "react-native-safe-area-context";
+
+const SafeAreaView = styled(RNSafeAreaView);
 
 export default function Index() {
   return (
     <>
-      <View className="flex-1 items-center justify-center bg-background ">
+      <SafeAreaView className="flex-1 bg-background p-5">
         <Text className="text-xl font-bold text-success">
           Welcome to Nativewind!
         </Text>
@@ -40,7 +45,7 @@ export default function Index() {
         >
           Claude Max Subscription
         </Link>
-      </View>
+      </SafeAreaView>
     </>
   );
 }
