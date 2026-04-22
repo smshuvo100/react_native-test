edbdf57e3aed735c64742deb85d85680e13e9938
smshuvo100 <smshuvo100@gmail.com>
2026-04-23 00:56:56 +0600

    3rd

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,50 +1,96 @@
-import { Link } from "expo-router";
+import ListHeading from "@/components/ListHeading";
+import SubscriptionCard from "@/components/SubscriptionCard";
+import UpcomingSubscription from "@/components/UpcomingSubscriptionCard";
+import {
+  HOME_BALANCE,
+  HOME_SUBSCRIPTIONS,
+  HOME_USER,
+  UPCOMING_SUBSCRIPTIONS,
+} from "@/constants/data";
+import { icons } from "@/constants/icons";
+import images from "@/constants/images";
+import { formatCurrency } from "@/lib/utils";
+import dayjs from "dayjs";
 import { styled } from "nativewind";
-import { Text } from "react-native";
+import { useState } from "react";
+import { FlatList, Image, Text, View } from "react-native";
 
 import { SafeAreaView as RNSafeAreaView } from "react-native-safe-area-context";
 
 const SafeAreaView = styled(RNSafeAreaView);
 
 export default function Index() {
+  const [expandedSubscriptionId, setExpandedSubscriptionId] = useState<
+    string | null
+  >(null);
   return (
     <>
       <SafeAreaView className="flex-1 bg-background p-5">
-        <Text className="text-xl font-bold text-success">
-          Welcome to Nativewind!
-        </Text>
+        <FlatList
+          ListHeaderComponent={() => (
+            <>
+              <View className="home-header">
+                <View className="home-user">
+                  <Image source={images.avatar} className="home-avatar" />
+                  <Text className="home-user-name">{HOME_USER.name}</Text>
+                </View>
 
-        <Link
-          href="/onboarding"
-          className="mt-4 rounded bg-primary text-white p-4"
-        >
-          Go to Onboarding
-        </Link>
+                <Image source={icons.add} className="home-add-icon" />
+              </View>
 
-        <Link
-          href="/(auth)/sing-in"
-          className="mt-4 rounded bg-primary text-white p-4"
-        >
-          Go to sign-in
-        </Link>
+              <View className="home-balance-card">
+                <Text className="home-balance-label">Balance</Text>
+                <View className="home-balance-row">
+                  <Text className="home-balance-amount">
+                    {formatCurrency(HOME_BALANCE.amount)}
+                  </Text>
+                  <Text className="home-balance-date">
+                    {dayjs(HOME_BALANCE.nextRenewalDate).format("MM/DD/YYYY")}
+                  </Text>
+                </View>
+              </View>
 
-        <Link
-          href="/(auth)/sign-up"
-          className="mt-4 rounded bg-primary text-white p-4"
-        >
-          Go to sign-up
-        </Link>
+              <View className="md-5">
+                <ListHeading title="Upcoming" />
+                <FlatList
+                  ListHeaderComponent={<View className="h-4" />}
+                  data={UPCOMING_SUBSCRIPTIONS}
+                  renderItem={({ item }) => <UpcomingSubscription {...item} />}
+                  keyExtractor={(item) => item.id}
+                  horizontal
+                  showsHorizontalScrollIndicator={false}
+                  ListEmptyComponent={
+                    <Text className="home-empty-state">
+                      No Upcoming renewals yet.
+                    </Text>
+                  }
+                />
+              </View>
 
-        <Link href="/subscriptions/spotify">Spotify Subscription</Link>
-
-        <Link
-          href={{
-            pathname: "/subscriptions/[id]",
-            params: { id: "claude" },
-          }}
-        >
-          Claude Max Subscription
-        </Link>
+              <ListHeading title="all Subscriptions" />
+            </>
+          )}
+          data={HOME_SUBSCRIPTIONS}
+          keyExtractor={(item) => item.id}
+          renderItem={({ item }) => (
+            <SubscriptionCard
+              {...item}
+              expanded={expandedSubscriptionId === item.id}
+              onPress={() =>
+                setExpandedSubscriptionId((currentId) =>
+                  currentId === item.id ? null : item.id,
+                )
+              }
+            />
+          )}
+          extraData={expandedSubscriptionId}
+          ItemSeparatorComponent={() => <View className="h-4" />}
+          showsVerticalScrollIndicator={false}
+          ListEmptyComponent={
+            <Text className="home-empty-state">No subscriptions yet.</Text>
+          }
+          contentContainerClassName="pb-30"
+        />
       </SafeAreaView>
     </>
   );
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,29 @@
 import "@/global.css";
+
+import { useFonts } from "expo-font";
 import { Stack } from "expo-router";
+import * as SplashScreen from "expo-splash-screen";
+import { useEffect } from "react";
 
 export default function RootLayout() {
+  const [fontLoaded] = useFonts({
+    "sans-regular": require("../assets/fonts/PlusJakartaSans-Regular.ttf"),
+    "sans-bold": require("../assets/fonts/PlusJakartaSans-Bold.ttf"),
+    "sans-medium": require("../assets/fonts/PlusJakartaSans-Medium.ttf"),
+    "sans-semibold": require("../assets/fonts/PlusJakartaSans-SemiBold.ttf"),
+    "sans-extrabold": require("../assets/fonts/PlusJakartaSans-ExtraBold.ttf"),
+    "sans-light": require("../assets/fonts/PlusJakartaSans-Light.ttf"),
+  });
+
+  useEffect(() => {
+    if (fontLoaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [fontLoaded]);
+
+  if (!fontLoaded) {
+    return null;
+  }
+
   return <Stack screenOptions={{ headerShown: false }} />;
 }
